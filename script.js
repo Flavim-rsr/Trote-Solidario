d7585f94eadcad8dbf21196208140fb0684a6f29
Lucas Gerbasi <126926247+lucasgerbasi@users.noreply.github.com>
2023-03-24 15:10:08 -0300

    Script Fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Mudança do site de escuro para claro
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle('light')
@@ -83,36 +83,4 @@ pauseButton.addEventListener("click", pauseSlider); // botão ativa a function "
   
 // Expand
 
-const headerBtn = document.querySelector('#alimentos-btn');
-const content = document.querySelector('#content1');
-
-headerBtn.addEventListener('click', () => {
-  const expanded = headerBtn.getAttribute('aria-expanded') === 'true' || false;
-  headerBtn.setAttribute('aria-expanded', !expanded);
-  content.hidden = expanded;
-});
-
-const headerBtn2 = document.querySelector('#pontuacao-btn');
-const content2 = document.querySelector('#content2');
-
-headerBtn2.addEventListener('click', () => {
-  const expanded = headerBtn2.getAttribute('aria-expanded') === 'true' || false;
-  headerBtn2.setAttribute('aria-expanded', !expanded);
-  content2.hidden = expanded;
-});
-const headerBtn3 = document.querySelector('#sangue-btn');
-const content3 = document.querySelector('#content3');
-
-headerBtn3.addEventListener('click', () => {
-  const expanded = headerBtn3.getAttribute('aria-expanded') === 'true' || false;
-  headerBtn3.setAttribute('aria-expanded', !expanded);
-  content3.hidden = expanded;
-});
-const headerBtn4 = document.querySelector('#calculadora-btn');
-const content4 = document.querySelector('#content4');
-
-headerBtn4.addEventListener('click', () => {
-  const expanded = headerBtn4.getAttribute('aria-expanded') === 'true' || false;
-  headerBtn4.setAttribute('aria-expanded', !expanded);
-  content4.hidden = expanded;
-});
+// eu tentei colocar o script expand aqui mas ele parou de funcionar e eu não sei porque
